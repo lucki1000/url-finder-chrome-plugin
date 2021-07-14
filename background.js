bf83af1c62c9a73b49360d541f102de355fd6d08
lucki1000 <ludwig.germersheim@gmail.com>
2021-07-14 08:33:13 +0200

    add regex to find vivo.st and vivo.sx links, also push all links to the popup.html, alert was removed

--- background.js
+++ background.js
@@ -1,7 +1,33 @@
-//Here my javascript code
+function DOMtoString(document_root) {
+    var html = '',
+        node = document_root.firstChild;
+    while (node) {
+        switch (node.nodeType) {
+            case Node.ELEMENT_NODE:
+                html += node.outerHTML;
+                break;
+            case Node.TEXT_NODE:
+                html += node.nodeValue;
+                break;
+            case Node.CDATA_SECTION_NODE:
+                html += '<![CDATA[' + node.nodeValue + ']]>';
+                break;
+            case Node.COMMENT_NODE:
+                html += '<!--' + node.nodeValue + '-->';
+                break;
+            case Node.DOCUMENT_TYPE_NODE:
+                // (X)HTML documents are identified by public identifiers
+                html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+                break;
+        }
+        node = node.nextSibling;
+    }
+    let str = new RegExp("(http|https):\\/\\/vivo.sx\\/..........", "g");
+    let result = html.match(str);
+    return result;
+}
 
-alert("Background is running")
-
-//var markup = document.documentElement.innerHTML;
-//alert(markup);
-
+chrome.runtime.sendMessage({
+    action: "getSource",
+    source: DOMtoString(document)
+});
--- content.js
+++
@@ -1,9 +0,0 @@
-// content.js
-
-//alert("HI")
-let markup = document.documentElement.innerHTML;
-let str = new RegExp("(http|https):\\/\\/vivo.sx\\/..........", "g");
-let result = markup.match(str);
-if (result.length != 0)
-    alert(result)
-    console.log(result)
